935b8a408bc40ccf09fb5a571c13ec864d2272d5
sarthakPabbly <sarthak.tiwari@pabbly.com>
2025-12-25 18:50:36 +0530

    resolving cors issue in production

--- backend/index.js
+++ backend/index.js
@@ -20,17 +20,16 @@ const app = express();
 const whitelist = [FRONTEND_URL];
 const corsOptions = {
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin && NODE_ENV === "development") {
-      return callback(null, true);
-    }
-    
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  if (!origin) {
+    return callback(null, true);
+  }
+
+  if (whitelist.includes(origin)) {
+    return callback(null, true);
+  }
+
+  callback(new Error('Not allowed by CORS'));
+},
   credentials: true,
   optionsSuccessStatus: 200
 };
